fba78df7825009133a4709448d5a5b4523ea7a69
Laura Felce <crepu83@yahoo.it>
2019-07-12 16:48:31 +0000

    Automatically backed up by Learn

--- app/models/note.rb
+++ app/models/note.rb
@@ -3,6 +3,7 @@ class Note < ApplicationRecord
   has_many :viewers
   has_many :readers, through: :viewers, source: :user
 
+    before_save :ensure_owner_can_read
 
     def visible_to=(my_readers)
       self.readers = my_readers.split(',').map do |name|
