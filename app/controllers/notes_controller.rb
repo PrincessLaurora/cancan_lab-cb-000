0d4db64cdf5bb024ab2d9b523100824241e265d0
Laura Felce <crepu83@yahoo.it>
2019-07-12 15:59:02 +0000

    Automatically backed up by Learn

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,6 +2,7 @@ class NotesController < ApplicationController
   load_and_authorize_resource only: [:edit, :show, :update]
 
     def index
+         @notes = Note.none
       if current_user
       @notes = current_user.readable
       end
