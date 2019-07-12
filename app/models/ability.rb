b65912036f5fcfd6e1a2692fe2fc5c02839ebbed
Laura Felce <crepu83@yahoo.it>
2019-07-12 14:09:22 +0000

    Automatically backed up by Learn

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,7 +5,9 @@ class Ability
 
   def initialize(user)
     # Define abilities for the passed in user here. For example:
-    can:read, Note 
+    can :read, Note do |note|
+      unless !note.readers
+
     #   user ||= User.new # guest user (not logged in)
     #   if user.admin?
     #     can :manage, :all
