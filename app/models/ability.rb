76ab7e41110c1a2b452e008c4049fea700466509
Laura Felce <crepu83@yahoo.it>
2019-07-12 15:45:20 +0000

    Automatically backed up by Learn

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,6 +8,18 @@ class Ability
     can :read, Note do |note|
       note.readers.include?(user)
     end
-    can :manage, Note, {user_id: user.id}
+    can :read, Note do |note|
+      note.user_id == user.id
+    end
+    can :update, Note do |note|
+       note.user_id == user.id
+    end     
   end
 end
+ #or 
+ #can :read, Note do |note|
+   #note.readers.include?(user)
+ #end
+ #can :manage, Note, {user_id: user.id}
+ #end
+#end
