9e36581ce1537132e4b2f5a32f6f5c4392835700
Laura Felce <crepu83@yahoo.it>
2019-07-12 16:54:38 +0000

    Automatically backed up by Learn

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -17,13 +17,14 @@ class NotesController < ApplicationController
       if current_user
       note = Note.new(note_params)
       note.save!
-      redirect_to '/'
       end
+      redirect_to '/'
     end
 
     def update
       if current_user
       @note.update(note_params)
+    end
       redirect_to '/'
       end
     end
