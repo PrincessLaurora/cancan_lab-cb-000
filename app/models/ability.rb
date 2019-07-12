f88244fb4c9262393809d0bfb7669647a8bfff7b
Laura Felce <crepu83@yahoo.it>
2019-07-12 14:52:04 +0000

    Automatically backed up by Learn

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,7 +6,7 @@ class Ability
   def initialize(user)
 
     can :read, Note do |note|
-      note.readers.include?(user)
+      note.readers.include?user
     can :read, Note do |note|
       note.user_id == user.id
 
