f3d1c9f9c9a92b40854bf52687a5dd9fc08ff70b
Laura Felce <crepu83@yahoo.it>
2019-07-12 16:46:29 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    @current_user ||= User.find_by(id: session[:user])
+  User.find_by(id: session[:user])
   end
 
 end
