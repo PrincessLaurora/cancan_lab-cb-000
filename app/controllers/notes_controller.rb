bcbb04cb502865b1579579a96826081cf8cf3be9
Laura Felce <crepu83@yahoo.it>
2019-07-12 16:05:23 +0000

    Automatically backed up by Learn

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,14 +2,9 @@ class NotesController < ApplicationController
   load_and_authorize_resource only: [:edit, :show, :update]
 
     def index
-      @notes = Note.none
-      if current_user
-      @notes = current_user.readable
-      end
     end
 
     def new
-      render partial: 'form', locals: {note: Note.new}
     end
 
     def create
